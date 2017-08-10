f0706940ebf8230f1d589ac85477cf6ba4cd8c64
dongqiankun <781739081@qq.com>
2017-08-10 16:34:07 +0800

    整洁代码

--- UserNotifyConsole/EventHandlers/TimeoutCancelOrderIntegrationEventHandler.cs
+++ UserNotifyConsole/EventHandlers/TimeoutCancelOrderIntegrationEventHandler.cs
@@ -1,19 +1,24 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using EventBus.Abstractions;
+using Microsoft.Extensions.Logging;
 using Order.Api.Events;
 
 namespace UserNotifyConsole.EventHandlers
 {
     public class TimeoutCancelOrderIntegrationEventHandler: IIntegrationEventHandler<TimeoutCancelOrderIntegrationEvent>
     {
+        private readonly ILogger<TimeoutCancelOrderIntegrationEventHandler> _logger;
+        public TimeoutCancelOrderIntegrationEventHandler(ILogger<TimeoutCancelOrderIntegrationEventHandler> logger)
+        {
+            _logger = logger;
+        }
+
         public Task Handle(TimeoutCancelOrderIntegrationEvent @event)
         {
-
-           return  Task.Run(() =>
-            {
-                Console.WriteLine(@event.OrderId);
-            });
+            _logger.LogInformation($"UserNotifyConsole received sub event：" + @event.Id);
+            return Task.CompletedTask;
         }
     }
 }
--- UserNotifyConsole/Program.cs
+++ UserNotifyConsole/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Threading.Tasks;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
@@ -21,13 +23,14 @@ namespace UserNotifyConsole
 
         public static void Main(string[] args)
         {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
             StartUp();
 
             IServiceCollection services = new ServiceCollection();
             var serviceProvider = ConfigureServices(services);
             ConfigureEventBus(serviceProvider);
-            var logger = serviceProvider.GetService<ILoggerFactory>();
 
+            var logger = serviceProvider.GetService<ILoggerFactory>();
             Configure(logger);
 
             Console.ReadKey();
@@ -45,46 +48,59 @@ namespace UserNotifyConsole
 
         public static IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            services
-                .AddLogging()
-                .AddOptions();
+            #region 日志注入
+            services.AddLogging()
+                       .AddOptions(); 
+            #endregion
 
+            #region rabbit connection接口注入
             services.AddSingleton<IRabbitMQPersistentConnection>(sp =>
-            {
-                var logger = sp.GetRequiredService<ILogger<DefaultRabbitMQPersistentConnection>>();
-
-                var factory = new ConnectionFactory()
                 {
-                    HostName = Configuration["EventBusConnection"]
-                };
+                    var logger = sp.GetRequiredService<ILogger<DefaultRabbitMQPersistentConnection>>();
 
-                return new DefaultRabbitMQPersistentConnection(factory, logger);
-            });
+                    var factory = new ConnectionFactory()
+                    {
+                        HostName = Configuration["EventBusConnection"]
+                    };
+
+                    return new DefaultRabbitMQPersistentConnection(factory, logger);
+                }); 
+            #endregion
 
             RegisterEventBus(services);
 
+            #region autofac ico
             var container = new ContainerBuilder();
             container.Populate(services);
-            return new AutofacServiceProvider(container.Build());
+            return new AutofacServiceProvider(container.Build()); 
+            #endregion
         }
 
+        #region 日志配置
         public static void Configure(ILoggerFactory loggerFactory)
         {
             loggerFactory
                 .AddConsole(Configuration.GetSection("Logging"))
                 .AddConsole(LogLevel.Debug);
         }
+        #endregion
+
+        #region eventbus订阅
         private static void ConfigureEventBus(IServiceProvider serviceProvider)
         {
             var eventBus = serviceProvider.GetRequiredService<IEventBus>();
             eventBus.Subscribe<TimeoutCancelOrderIntegrationEvent, TimeoutCancelOrderIntegrationEventHandler>();
         }
+        #endregion
+
+        #region eventbus相关注入
         private static void RegisterEventBus(IServiceCollection services)
         {
             services.AddSingleton<IEventBus, EventBusRabbitMQ>();
             services.AddSingleton<IEventBusSubscriptionsManager, InMemoryEventBusSubscriptionsManager>();
             services.AddTransient<TimeoutCancelOrderIntegrationEventHandler>();
 
-        }
+        } 
+        #endregion
     }
 }
